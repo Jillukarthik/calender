68103b68feb4f5b4afef01ead102f60aceeb8e75
karthik <2001karthicksrini@gmail.com>
2023-06-07 12:14:57 +0530

    add string literals

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-var currentYear;
-var currentMonth;
+let currentYear;
+let currentMonth;
 
 function generateCalendar(year, month) {
-  var calendar = document.getElementById("calendar");
+  const calendar = document.getElementById("calendar");
 
-  var daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-  var calendarHTML = '';
+  let calendarHTML = '';
 
-  var date = new Date(year, month);
-  var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-  var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+  const date = new Date(year, month);
+  const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
-  calendarHTML += '<div class="calendar">';
-  calendarHTML += '<h2>' + months[date.getMonth()] + ' ' + date.getFullYear()  +'</h2>';
-  calendarHTML += '<table>';
-  calendarHTML += '<tr>';
+  calendarHTML += `<div class="calendar">`;
+  calendarHTML += `<h2>${months[date.getMonth()]} ${date.getFullYear()}</h2>`;
+  calendarHTML += `<table>`;
+  calendarHTML += `<tr>`;
 
-  for (var i = 0; i < daysOfWeek.length; i++) {
-    calendarHTML += '<th>' + daysOfWeek[i] + '</th>';
+  for (let i = 0; i < daysOfWeek.length; i++) {
+    calendarHTML += `<th>${daysOfWeek[i]}</th>`;
   }
 
-  calendarHTML += '</tr>';
+  calendarHTML += `</tr>`;
 
-  var dayCount = 1;
-  var rowCount = 0;
+  let dayCount = 1;
+  let rowCount = 0;
 
-  for (var i = 0; i < 6; i++) {
-    calendarHTML += '<tr>';
+  for (let i = 0; i < 6; i++) {
+    calendarHTML += `<tr>`;
 
-    for (var j = 0; j < 7; j++) {
+    for (let j = 0; j < 7; j++) {
       if (rowCount === 0 && j < firstDay.getDay()) {
-        calendarHTML += '<td></td>';
+        calendarHTML += `<td></td>`;
       } else if (dayCount > lastDay.getDate()) {
-        calendarHTML += '<td></td>';
+        calendarHTML += `<td></td>`;
       } else {
         if (
           dayCount === currentDate.getDate() &&
           date.getMonth() === currentDate.getMonth() &&
           date.getFullYear() === currentDate.getFullYear()
         ) {
-          calendarHTML += '<td class="current-day">' + dayCount + '</td>';
+          calendarHTML += `<td class="current-day">${dayCount}</td>`;
         } else {
-          calendarHTML += '<td>' + dayCount + '</td>';
+          calendarHTML += `<td>${dayCount}</td>`;
         }
         dayCount++;
       }
     }
-    calendarHTML += '</tr>';
+    calendarHTML += `</tr>`;
     rowCount++;
 
     if (dayCount > lastDay.getDate()) {
@@ -56,15 +56,15 @@ function generateCalendar(year, month) {
     }
   }
 
-  calendarHTML += '</table>';
+  calendarHTML += `</table>`;
 
   // Add navigation buttons
-  calendarHTML += '<div class="navigation">';
-  calendarHTML += '<button onclick="previousMonth()">&lt;</button>';
-  calendarHTML += '<button onclick="nextMonth()">&gt;</button>';
-  calendarHTML += '</div>';
+  calendarHTML += `<div class="navigation">`;
+  calendarHTML += `<button onclick="previousMonth()">&lt;</button>`;
+  calendarHTML += `<button onclick="nextMonth()">&gt;</button>`;
+  calendarHTML += `</div>`;
 
-  calendarHTML += '</div>';
+  calendarHTML += `</div>`;
   calendar.innerHTML = calendarHTML;
   currentYear = year;
   currentMonth = month;
@@ -88,6 +88,5 @@ function nextMonth() {
   generateCalendar(currentYear, currentMonth);
 }
 
-// Initial generation of calendar for current month
-var currentDate = new Date();
+const currentDate = new Date();
 generateCalendar(currentDate.getFullYear(), currentDate.getMonth());
